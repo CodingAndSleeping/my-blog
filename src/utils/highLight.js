6ad877f7c9956365cabbb5c1bbceee246182d490
Lizhitao <1162300237@qq.com>
2022-06-15 14:14:35 +0800

    master

--- src/utils/highLight.js
+++ src/utils/highLight.js
@@ -10,14 +10,14 @@ Highlight.install = function (Vue) {
     // 自定义指令 v-highlight
     Vue.directive('highlight', {
         // 被绑定元素插入父节点时调用
-        inserted: function(el) {
+        inserted(el) {
             let blocks = el.querySelectorAll('pre code');
             for (let i = 0; i < blocks.length; i++) {
                 Hljs.highlightBlock(blocks[i]);
             }
         },
         // 指令所在组件的 VNode 及其子 VNode 全部更新后调用
-        componentUpdated: function(el) {
+        componentUpdated(el) {
             let blocks = el.querySelectorAll('pre code');
             for (let i = 0; i < blocks.length; i++) {
                 Hljs.highlightBlock(blocks[i]);
